d0c22416106887421fd949b5074aa4a94f6045f8
iuriv <iurivintonyak@gmail.com>
2023-10-10 16:42:22 -0400

    Updates simulation to use rand

--- simulation.py
+++ simulation.py
@@ -1,14 +1,13 @@
+from rand import rand
 time_list = []
 ticket_list = []
 
 # Call loop
-def run(x: float = .5, agent: float = .6):
+def run(x: float = .5, iterator: int = 50):
     calls = 0
     ticket = False
     time_sec = 0.0
     result = []
-    x = x
-    agent = agent
     for calls in range(0,3):
         time_sec += 3
         # x = 0 # Switchboard function
@@ -16,7 +15,8 @@ def run(x: float = .5, agent: float = .6):
         if x < 1.5:
             time_sec += (x * 60)
             # Connect to agent
-            #agent = 0.0 # Equal to rand func
+            agent = rand(iterator) # Equal to rand func
+            print(agent)
             time_sec += 5
             if agent <= 0.2 and agent >= 0.0:
                 time_sec += 72
@@ -42,5 +42,9 @@ def run(x: float = .5, agent: float = .6):
     result.append(ticket)
     return result
 
-print(run())
-
+print(run(.5, 20))
+print(run(.5, 100))
+print(run(.5, 1))
+print(run(.5, 99))
+print(run(.5, 78))
+print(run(1.5, 78))
